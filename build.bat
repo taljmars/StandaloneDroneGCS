582928e171edae7b3411a5c3b17f604a282c7a5d
taljmars <taljmars@gmail.com>
2018-03-26 06:52:30 +0300

    Update building script

--- build.bat
+++ build.bat
@@ -9,12 +9,20 @@ git.exe fetch --no-tags --progress https://github.com/taljmars/StandaloneDroneGC
 echo --------------- Windows 64 --------------
 del Windows64\*.zip
 echo f | xcopy /f /y ..\DroneGCS\ClientInstallation\Windows\Console-*-win-x64.zip Windows64\Console-1.%arg1%-win-x64.zip
+echo f | xcopy /f /y ..\DroneServer\ServerInstallation\Windows64\ServerCore-*-win-x64.zip Windows64\ServerCore-1.%arg1%-win-x64.zip
+
+C:\Users\taljmars\Workspace\
 
 echo --------------- Linux --------------
+
+
+echo --------------- Pushing --------------
+
+@ECHO Build-%arg1% > version
 
 git status
 git add *
 git commit -m "Building standalone package for Build-%arg1%"
 git status
 
-@ECHO "Building standalone package for Build-%arg1%" > version
+@ECHO Done
