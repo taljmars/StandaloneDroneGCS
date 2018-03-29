dd9d308316ef373b85e8f4e8f2f7584a8ed48745
taljmars <taljmars@gmail.com>
2018-03-29 23:05:19 +0300

    .

--- Windows64-lite/install.bat
+++ Windows64-lite/install.bat
@@ -24,17 +24,17 @@ echo call run.bat >> %RUNFILE%
 echo cd .. >> %RUNFILE%
 echo echo "Done"  >> %RUNFILE%
 
-set RUNFILE="run-test.bat"
+set RUNFILE="run-auto-tester.bat"
 
 echo "Creating single run-test file"
 echo @echo off > %RUNFILE%
 echo cd ServerCore >> %RUNFILE%
 echo echo Loading Server #1 >> %RUNFILE%
-echo start /b cmd /c run.bat >> %RUNFILE%
+echo start /b cmd /c run.bat &>&> server-test.log >> %RUNFILE%
 echo echo Loading Server #2 >> %RUNFILE%
 echo timeout /t 7 /nobreak >> %RUNFILE%
 echo cd ../DroneConsole >> %RUNFILE%
-echo call run-test.bat >> %RUNFILE%
+echo call run-tester.bat >> %RUNFILE%
 echo cd .. >> %RUNFILE%
 echo echo "Done"  >> %RUNFILE%
 
