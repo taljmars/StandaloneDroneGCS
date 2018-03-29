a458b055ad6419223de4ba9f93aa33ce3007e42b
taljmars <taljmars@gmail.com>
2018-03-29 22:46:54 +0300

    update tester

--- Windows64-lite/install.bat
+++ Windows64-lite/install.bat
@@ -24,6 +24,20 @@ echo call run.bat >> %RUNFILE%
 echo cd .. >> %RUNFILE%
 echo echo "Done"  >> %RUNFILE%
 
+set RUNFILE="run-test.bat"
+
+echo "Creating single run-test file"
+echo @echo off > %RUNFILE%
+echo cd ServerCore >> %RUNFILE%
+echo echo Loading Server #1 >> %RUNFILE%
+echo start /b cmd /c run.bat >> %RUNFILE%
+echo echo Loading Server #2 >> %RUNFILE%
+echo timeout /t 7 /nobreak >> %RUNFILE%
+echo cd ../DroneConsole >> %RUNFILE%
+echo call run-test.bat >> %RUNFILE%
+echo cd .. >> %RUNFILE%
+echo echo "Done"  >> %RUNFILE%
+
 :: Setting uninstall script
 set UNINSTFILE="uninstall.bat"
 
